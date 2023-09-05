5bbbd51af069e19bb146b068de21c3c03848ad24
Aashish Gupta <aashishkumar013@gmail.com>
2023-09-05 15:35:04 +0100

    added license header

--- forceChains-main/src/chainsIo.h
+++ forceChains-main/src/chainsIo.h
@@ -1,3 +1,24 @@
+/* 
+# Force Chain Finder - A software tool for the recursive detection of
+# force-chains in granular materials via minor principal stress.
+# Copyright (C) 2023 Omid Ejtehadi 'oejtehad@ed.ac.uk'
+#		             Aashish K Gupta 'A.K.Gupta-2@sms.ed.ac.uk'
+#		             Sorush Khajepor 'sorush.kh@gmail.com'
+#		             Sina Haeri 'shaeri@ed.ac.uk'
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include "particle.h"
 
